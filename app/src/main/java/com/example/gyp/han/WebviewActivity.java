02a1ef8da98e9262d1375979742cf2a2a9b52f73
skyouri <skyouri@naver.com>
2016-08-08 21:50:48 +0900

    안드로이드 통신 미구현

--- app/src/main/java/com/example/gyp/han/WebviewActivity.java
+++ app/src/main/java/com/example/gyp/han/WebviewActivity.java
@@ -14,7 +14,7 @@ public class WebviewActivity extends AppCompatActivity {
         webView = (WebView)findViewById(R.id.webView);
         webView.loadUrl("http://아이피주소:8080/test4/main.jsp"); //본인 아이피 입력
         //웹뷰 출력후 회원가입 버튼이 안눌림...아이디 : park 비번 : 1234 넣고 로그인 한후 모든 웹기능 사용 가능 함
-
+        //웹뷰 화면
 
 
 
